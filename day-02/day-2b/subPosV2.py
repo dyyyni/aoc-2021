47dab84a36eb5c3069e38087859c2bd8f2ebd92f
dyyyni <luomadaniel95@gmail.com>
2021-12-11 22:31:40 +0200

    day-02 complete

--- day-02/day-2b/subPosV2.py
+++ day-02/day-2b/subPosV2.py
@@ -1,19 +1,19 @@
-
-
 with open("input.txt") as file:
     movesRaw = [move.strip().split(' ') for move in file.readlines()]
     moves = list((zip([x[0] for x in movesRaw], map(lambda n: int(n[1]), movesRaw))))
 
 x = 0
 y = 0 # down is positive
+aim = 0
 
 for move in moves:
     if move[0] == 'forward':
         x += move[1]
+        y = y + aim*move[1]
     elif move[0] == 'up':
-        y -= move[1]
+        aim -= move[1]
     elif move[0] == 'down':
-        y += move[1]
+        aim += move[1]
 
 print(x,y)
 print(x*y)
